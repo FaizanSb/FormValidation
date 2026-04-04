e00918e2a6df0e8eb184f9a8cf8e1d283ad589fd
= <=>
2026-04-04 12:40:45 +0500

    Now page login becomes successfull

---
+++ FrontEnd/login.js
@@ -0,0 +1,41 @@
+
+let form = document.getElementById('form');
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    let email = document.getElementById('email').value.trim();
+    let password = document.getElementById('password').value.trim();
+
+    let error = document.getElementById('error');
+    if (email === '' || password === '') {
+        error.textContent = 'All fields are required.';
+        console.log('All fields are required.');
+    } else if (!email.includes("@")) {
+        error.textContent = 'Please enter a valid email address.';
+        console.log('Please enter a valid email address.');
+    } else {
+
+        try {
+            const res = await fetch('http://localhost:5000/api/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email,
+                    password
+                })
+            })
+            const data = await res.json();
+            if (data.success) {
+                window.location.href = '/dashboard.html';
+            } else {
+                error.textContent = data.message;
+            }
+        } catch (err) {
+            console.log("Error in fetching data", err);
+        }
+    }
+
+})
+
